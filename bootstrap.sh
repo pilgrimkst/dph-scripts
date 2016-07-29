6aabd9750b91ad7a2960db9909b3d4795959707f
Andrey Kuznetsov <andrey198701@gmail.com>
2016-07-29 11:03:28 +0300

    add package replace

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,4 +19,13 @@ cd - && mv /tmp/project-bootstrap ${project}
 
 cd ./${project} && git init && git add . && git remote add origin git@bitbucket.org:depositphotos/${project}.git
 
+mv ./services/_bootstrap_ ./services/${project}-app
+
+mv ./services/${project}-app/src/main/java/com/dph/_bootstrap_ ./services/${project}-app/src/main/java/com/dph/${project}
+
+sed s/_bootstrap_/${project}-app/g settings.gradle
+
+find . -name "*.java" | while read fname; do cat $fname |  sed  s/_bootstrap_/_XXXX_/g > ${fname}_tmp; mv ${fname}_tmp $fname; done
+
 echo ${project}
+
