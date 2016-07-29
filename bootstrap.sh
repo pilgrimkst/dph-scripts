1095e7dc3d14926d712b14f5a865756433a5db07
Andrey Kuznetsov <andrey198701@gmail.com>
2016-07-29 11:31:23 +0300

    add debug text

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,25 +11,30 @@ while [ "$1" != "" ]; do
     shift
 done
 
+exprot current_dir = pwd
 cd /tmp
+
+echo "Fetching project template"
 
 rm -rf /tmp/project-bootstrap && git clone git@bitbucket.org:depositphotos/project-bootstrap.git && rm -rf /tmp/project-bootstrap/.git
 
-cd - && mv /tmp/project-bootstrap ${project}
-
+echo "Moving template to working folder ${current_dir}"
+cd ${current_dir} && mv /tmp/project-bootstrap ${project}
 cd ./${project} && git init && git add . && git remote add origin git@bitbucket.org:depositphotos/${project}.git
 
+echo "Replacing template with ${project}"
+echo "folders"
 mv ./services/_bootstrap_ ./services/${project}-app
-
 mv ./services/${project}-app/src/main/java/com/dph/_bootstrap_ ./services/${project}-app/src/main/java/com/dph/${project}
 
+echo "settings.gradle"
 sed s/_bootstrap_/${project}-app/g ./settings.gradle
+cat ./settings.gradle
 
+echo "Java classes"
 find . -name "*.java" | while read fname; do cat $fname |  sed  s/_bootstrap_/${project}/g > ${fname}_tmp; mv ${fname}_tmp $fname; done
 
+echo "Building project"
 chmod +x ./gradlew
 
 ./gradlew build
-
-echo ${project}
-
